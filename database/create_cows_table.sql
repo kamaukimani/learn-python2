71d4dedb9e59b3e64c70b1647dc5cdf826a24eca
kamaukimani <paulkimys285@gmail.com>
2026-08-20 03:57:28 +0300

    execute sql from a file

---
+++ database/create_cows_table.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE cows(
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     color TEXT
+-- );
+--ALTER TABLE cows ADD COLUMN breed TEXT;
+-- INSERT INTO  cows(name,color,breed)
+-- VALUES('Bessie','dark-brown','Jersey')
+SELECT * FROM cows;
