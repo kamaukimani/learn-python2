02c46ffc8dceaf2401e9889a9068904c71ec5af4
kamaukimani <paulkimys285@gmail.com>
2026-08-22 10:57:33 +0300

    add INNER and LEFT JOIN query statements

--- Pets.session.sql
+++ Pets.session.sql
@@ -13,6 +13,24 @@
 -- );
 --ALTER TABLE cats ADD COLUMN owner_id INTEGER;
 --INSERT INTO owners (name) VALUES ('mugumogu');
-UPDATE cats SET owner_id=1 WHERE name='Maru';
-UPDATE cats SET owner_id=1 WHERE name='Hana';
-SELECT * FROM cats WHERE owner_id=1;
+-- UPDATE cats SET owner_id=1 WHERE name='Maru';
+-- UPDATE cats SET owner_id=1 WHERE name='Hana';
+-- SELECT * FROM cats WHERE owner_id=1;
+--INSERT INTO owners (name) VALUES ('Sophie');
+-- INSERT INTO cats (name,age,breed,owner_id)
+-- VALUES('Nona',4,'Tortoise',2);
+-- INSERT INTO cats (name,age,breed)
+-- VALUES('Lil''Bub',2,'perma-kitten');
+
+--INNER JOIN
+SELECT cats.name,cats.breed, owners.name AS 'owner_name'
+FROM cats
+INNER JOIN owners
+ON cats.owner_id=owners.id;
+
+--LEFT JOIN
+SELECT cats.name,cats.breed,owners.name AS 'owner_name'
+FROM cats
+LEFT OUTER JOIN owners     --use either LEFT JOIN OR LEFT OUTER JOIN
+ON cats.owner_id=owners.id;
+
