9cacd10947b394685ee1f4500eb4c7aa172ec7d1
kamaukimani <paulkimys285@gmail.com>
2026-08-24 16:20:06 +0300

    map python class and instances to database table and rows

---
+++ database/orm/department/department.py
@@ -0,0 +1,75 @@
+from __init__ import CURSOR,CONN
+class Department:
+    def __init__(self,name,location,id=None):
+        self.id=id
+        self.name=name
+        self.location=location
+    def __repr__(self):
+        return f"<Department {self.id}: {self.name}, {self.location}>"
+    @classmethod
+    def create_table(cls):
+        sql="""
+        CREATE TABLE IF NOT EXISTS departments(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        location TEXT
+        )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    @classmethod
+    def drop_table(cls):
+        sql="""
+        DROP TABLE IF EXISTS departments;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    def save(self):
+        sql="""
+        INSERT INTO departments (name,location) 
+        VALUES(?,?)
+        """
+        CURSOR.execute(sql,(self.name,self.location))
+        CONN.commit()
+        self.id=CURSOR.lastrowid
+    @classmethod
+    def create(cls,name,location):
+        department=cls(name,location)
+        department.save()
+        return department
+    def update(self):
+        sql="""
+        UPDATE departments
+        SET name=?,location=?
+        WHERE id=?
+        """
+        CURSOR.execute(sql,(self.name,self.location,self.id))
+        CONN.commit()
+    def delete(self):
+        sql="""
+        DELETE FROM departments
+        WHERE id=?
+        """
+        CURSOR.execute(sql,(self.id,))
+        CONN.commit()
+Department.create_table()
+#Department.drop_table()
+payroll=Department.create("Payroll","Building A, 5th floor")
+payroll.delete()
+hr=Department.create("Human Resources","Building C, East Wing")
+hr.delete()
+#payroll.delete()
+
+developer=Department("Developer","Nairobi,KENYA")
+
+#developer.delete()
+# print(developer)
+developer.save()
+developer.delete()
+developer.name="Software Engineer"
+developer.location="Nairobi-KENYA"
+print(f"developer id:{developer.id}")
+developer.update()
+# print(developer)
+CURSOR.execute("SELECT * FROM departments")
+print(CURSOR.fetchall())
